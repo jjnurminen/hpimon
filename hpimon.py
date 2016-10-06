cbd41bf5675faf5202a46f38edb106b8868faa32
Jussi Nurminen <jnu@iki.fi>
2016-10-06 09:22:28 +0300

    skeleton gui + timer

--- hpimon.py
+++ hpimon.py
@@ -9,6 +9,7 @@ Created on Thu Oct  6 09:03:58 2016
 from __future__ import print_function
 import sys
 from PyQt4 import QtGui, QtCore, uic
+import time
 
 
 class HPImon(QtGui.QMainWindow):
@@ -17,6 +18,16 @@ class HPImon(QtGui.QMainWindow):
         super(self.__class__, self).__init__()
         # load user interface made with designer
         uic.loadUi('hpimon.ui', self)
+
+        self.btnQuit.clicked.connect(self.close)
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(1000)
+    
+    
+    def update(self):
+        self.label_1.setText(str(time.clock()))
 
 
 def main():
