ea5b1ed9d121ed1428db21b42f79d90279f834a5
Jussi Nurminen <jnu@iki.fi>
2016-11-14 16:42:41 +0200

    packaging wip

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 15:50:55 2016
+
+@author: jussi
+"""
+
+from setuptools import setup
+
+
+setup(name='hpimon',
+      version='0.1',
+      description = 'Continuous HPI monitor for Elekta MEG systems',
+      author='Jussi Nurminen',
+      author_email = 'jnu@iki.fi',
+      license='MIT',
+      zip_safe=False)
