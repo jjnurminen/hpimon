4320db9d607292fa2f96ae70148d4630f13bb2d1
Jussi Nurminen <jnu@iki.fi>
2016-12-15 10:03:55 +0200

    pip install works

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ Created on Mon Nov 14 15:50:55 2016
 @author: jussi
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='hpimon',
-      version='0.1',
+      version='0.12.2',
       description='Continuous HPI monitor for Elekta MEG systems',
       author='Jussi Nurminen',
       author_email='jnu@iki.fi',
       license='MIT',
       url='https://github.com/jjnurminen/hpimon',
+      packages=find_packages(),
       include_package_data=True,
       entry_points={
-          'gui_scripts': ['hpimon = hpimon.hpimon:main']
+          'gui_scripts': ['hpimon = hpimon:main']
       })
